7e3e6190d96f9b41b0ae67d386ea5a6dc95741c3
Isaac Isvaldo Bunga <isaac.bunga@mamboo.co.ao>
2024-03-11 18:27:36 +0100

    update data

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import userRouter from "../modules/students/students.routes";
+
 import funcionarioRouter from "../modules/instructor/instructor.routes";
 import adminRouter from "../modules/admin/admin.routes";
 import { authenticateToken, checkAdminRole } from "../utils/middlewares/session";
 
 const routes = Router()
-routes.use('/user',userRouter)
+
 routes.use('/funcionario',funcionarioRouter)
 routes.use('/admin',authenticateToken,adminRouter)
 
